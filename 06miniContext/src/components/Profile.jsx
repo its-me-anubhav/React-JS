0a41d2d3850c36b6182a210c6843f381e6a7a358
its-me-anubhav <anubhavshakya543@gmail.com>
2024-12-13 17:15:38 +0530

    customReact

---
+++ 06miniContext/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+export function Profile() {
+    const {user} = useContext(UserContext)
+
+   if(!user)
+    return <div>Please login</div>
+    return <div>Welcome {user.username}</div>
+}
